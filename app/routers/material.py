a0870c99ad190ed4828b9cb939f984dd53e84aa3
Igor Duvanov <duvanovigor79@gmail.com>
2023-05-06 15:41:25 +0300

    додав сортування за роком для матеріалів

--- app/routers/material.py
+++ app/routers/material.py
@@ -7,6 +7,7 @@ from fastapi.responses import HTMLResponse
 from app.database import get_db
 from app.models.material import Material
 from app.templates import templates
+from sqlalchemy import extract
 
 router = APIRouter()
 
@@ -56,7 +57,7 @@ async def create_material(material: MaterialCreate, db: Session = Depends(get_db
 
 @router.get("/material_page", response_class=HTMLResponse)
 async def read_material_page(request: Request, db: Session = Depends(get_db), search: Optional[str] = None,
-                             sort_by: Optional[str] = None):
+                             sort_by: Optional[str] = None, year: Optional[int] = None):
     materials = db.query(Material)
 
     if search:
@@ -68,10 +69,13 @@ async def read_material_page(request: Request, db: Session = Depends(get_db), se
         elif sort_by == "weight_desc":
             materials = materials.order_by(Material.weight.desc())
 
+    if year:
+        materials = materials.filter(extract('year', Material.date_created) == year)
+
     materials = materials.all()
 
     return templates.TemplateResponse("material_page.html",
-                                      {"request": request, "materials": materials, "search": search, "sort_by": sort_by})
+                                      {"request": request, "materials": materials, "search": search, "sort_by": sort_by, "year": year})
 
 @router.put("/material/{material_id}")
 async def update_material(material_id: int, material: MaterialUpdate, db: Session = Depends(get_db)):

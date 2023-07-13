e714aea1ff3de2afc1b82a2f704090dc47a9cb06
Igor Duvanov <duvanovigor79@gmail.com>
2023-07-13 14:55:29 +0300

    додавання даних 1 поле

--- app/workshop/workshop_routers.py
+++ app/workshop/workshop_routers.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-from typing import Optional, List
-from fastapi import APIRouter, Request, Depends, HTTPException, status, Body
+from typing import Optional
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from app.routers.dependencies import get_current_user
 from app.models.users import Users
@@ -10,7 +9,7 @@ from app.templates import templates
 from app.models import Robitnuk, Operation, Odvumir, Oper
 from app.models.denzvit import Denzvit
 from pydantic import BaseModel
-from sqlalchemy.exc import SQLAlchemyError
+
 
 router = APIRouter()
 
@@ -21,42 +20,24 @@ class DenzvitBase(BaseModel):
     id_odvumir: Optional[int] = None
 
 
-class DenzvitCreate(BaseModel):
-    id_robitnuk: List[int]
-    id_operation: List[int]
-    kilkist: List[int]
-    id_odvumir: List[Optional[int]]
-
+class DenzvitCreate(DenzvitBase):
+    pass
 
 def create_denzvit_in_db(denzvit: DenzvitCreate, db: Session):
-    new_denzvits = []
-    for i in range(len(denzvit.id_robitnuk)):
-        new_denzvit = Denzvit(id_robitnuk=denzvit.id_robitnuk[i], id_operation=denzvit.id_operation[i],
-                              kilkist=denzvit.kilkist[i], id_odvumir=denzvit.id_odvumir[i])
-        db.add(new_denzvit)
-        new_denzvits.append(new_denzvit)
-    try:
-        db.commit()
-    except SQLAlchemyError as e:
-        print("Failed to add new denzvit to the database.")
-        print(e)
-        db.rollback()
-        return []
-    for new_denzvit in new_denzvits:
-        db.refresh(new_denzvit)
-    return new_denzvits
+    new_denzvit = Denzvit(id_robitnuk=denzvit.id_robitnuk, id_operation=denzvit.id_operation,
+                          kilkist=denzvit.kilkist, id_odvumir=denzvit.id_odvumir)
+    db.add(new_denzvit)
+    db.commit()
+    db.refresh(new_denzvit)
+    return new_denzvit
 
-@router.post("/workshop/workshop_denzvit_form", status_code=status.HTTP_201_CREATED)
-async def process_denzvit_form(
-    denzvit: DenzvitCreate = Body(...),
-    db: Session = Depends(get_db)
-    ):
-    new_denzvits = create_denzvit_in_db(denzvit, db)
-    return new_denzvits
-
+@router.post("/workshop/workshop_denzvit_form")
+async def create_denzvit(denzvit: DenzvitCreate, db: Session = Depends(get_db)):
+    new_denzvit = create_denzvit_in_db(denzvit, db)
+    return new_denzvit
 
 @router.get("/workshop_denzvit_form", response_class=HTMLResponse)
-async def read_index(request: Request, current_user: Users = Depends(get_current_user), db: Session = Depends(get_db)):
+async def read_index(request: Request, db: Session = Depends(get_db)):
     robitnuks = db.query(Robitnuk).all()
     operations = db.query(Operation).join(Oper).all()
     odvumirs = db.query(Odvumir).all()

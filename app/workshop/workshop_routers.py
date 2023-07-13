45b3ac67b57f032ff035d121f117edf0cf67dd41
Igor Duvanov <duvanovigor79@gmail.com>
2023-07-13 17:09:19 +0300

    додає тільки 1

--- app/workshop/workshop_routers.py
+++ app/workshop/workshop_routers.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict, List
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from app.routers.dependencies import get_current_user
@@ -10,31 +10,28 @@ from app.models import Robitnuk, Operation, Odvumir, Oper
 from app.models.denzvit import Denzvit
 from pydantic import BaseModel
 
-
 router = APIRouter()
 
 class DenzvitBase(BaseModel):
-    id_robitnuk: int
-    id_operation: int
-    kilkist: int
-    id_odvumir: Optional[int] = None
-
+    id_robitnuk: List[int]
+    id_operation: List[int]
+    kilkist: List[int]
+    id_odvumir: List[Optional[int]]
 
 class DenzvitCreate(DenzvitBase):
     pass
 
 def create_denzvit_in_db(denzvit: DenzvitCreate, db: Session):
-    new_denzvit = Denzvit(id_robitnuk=denzvit.id_robitnuk, id_operation=denzvit.id_operation,
-                          kilkist=denzvit.kilkist, id_odvumir=denzvit.id_odvumir)
-    db.add(new_denzvit)
+    for i in range(len(denzvit.id_robitnuk)):
+        new_denzvit = Denzvit(id_robitnuk=denzvit.id_robitnuk[i], id_operation=denzvit.id_operation[i],
+                              kilkist=denzvit.kilkist[i], id_odvumir=denzvit.id_odvumir[i])
+        db.add(new_denzvit)
     db.commit()
-    db.refresh(new_denzvit)
-    return new_denzvit
 
 @router.post("/workshop/workshop_denzvit_form")
 async def create_denzvit(denzvit: DenzvitCreate, db: Session = Depends(get_db)):
-    new_denzvit = create_denzvit_in_db(denzvit, db)
-    return new_denzvit
+    create_denzvit_in_db(denzvit, db)
+    return {"status": "success"}
 
 @router.get("/workshop_denzvit_form", response_class=HTMLResponse)
 async def read_index(request: Request, db: Session = Depends(get_db)):
